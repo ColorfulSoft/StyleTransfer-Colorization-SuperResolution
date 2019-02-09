78159168199528588a3271953fa44b75c13fa59d
Глеб Брыкин <glebbrykin@yandex.ru>
2019-02-09 17:14:49 +0300

    Add files via upload

---
+++ Style Transfer/2017. Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization/Implementation/src/Properties.cs
@@ -0,0 +1,17 @@
+﻿//***********************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//***********************************************
+
+//-> Подпись сборки.
+
+using System.Reflection;
+
+    [assembly: AssemblyTitle("Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization")]
+    [assembly: AssemblyDescription("Image style transfer")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyVersion("0.09.02.2018")]
+    [assembly: AssemblyCompany("ColorfulSoft")]
+    [assembly: AssemblyProduct("StyleImitator")]
+    [assembly: AssemblyCopyright("© Брыкин Глеб Сергеевич")]
+    [assembly: AssemblyTrademark("ImageInstruments")]
+    [assembly: AssemblyCulture("")]
