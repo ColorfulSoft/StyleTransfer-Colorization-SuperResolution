635974ae42ec94ab35b9dc630e489e62d31441b8
Глеб Брыкин <glebbrykin@yandex.ru>
2019-02-09 22:06:28 +0300

    Add files via upload

---
+++ Style Transfer/2015. A Neural Algorithm of Artistic Style/Implementation/src/Network/Optimization/Losses.cs
@@ -0,0 +1,85 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//*************************************************************************************************
+
+using System;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NeuralArt
+{
+
+    ///<summary>Функции потерь(вычисления градиентов).</summary>
+    public static class Losses
+    {
+
+        ///<summary>Функция взвешенной ошибки стиля изображения. L(style) = W(G(F(X)) - G(F(S))).</summary>
+        ///<param name="X">Карта признаков на выходе со слоя. Градиенты будут записаны в DW тензора.</param>
+        ///<param name="S">Матрица Грама для выходной карты признаков со слоя.</param>
+        ///<param name="k">Вес ошибки.</param>
+        public static float StyleLoss(Tensor X, Tensor S, float k)
+        {
+            var _Outp = Math.Gram_Matrix(X);
+            var _Dest = S;
+            double Temp = 0.0;
+            var D = new Tensor(_Outp.Width, _Outp.Height, 1, false);
+            for(int i = 0; i < _Outp.W.Length; i++)
+            {
+                var Delta = (_Outp.W[i] - _Dest.W[i]) * k;
+                Temp += Delta * Delta;
+                D.W[i] = Delta;
+            }
+            var M = X.Width * X.Height;
+            var N = X.Depth;
+            var Result = (float)((double)Temp / ((long)4 * M * M * N * N));
+            var Ft = Math.Transpose2D(Math.Flat(X));
+            var Grad = Math.MatMul2D(D, Ft);
+            Parallel.For(0, X.Depth, (int _d) =>
+            {
+                int i = 0;
+                for(int y = 0; y < X.Height; y++)
+                {
+                    for(int x = 0; x < X.Width; x++)
+                    {
+                        X.AddDW(x, y, _d, (float)((double)Grad.GetW(_d, i, 0) / ((long)M * M * N * N)));
+                        i += 1;
+                    }
+                }
+            });
+            return Result;
+        }
+
+        ///<summary>Функция взвешенной ошибки контента. L(content) = W(X - C)</summary>
+        ///<param name="X">Карта признаков на выходе со слоя. Градиенты будут записаны в DW тензора.</param>
+        ///<param name="C">Требуемая карта признаков для изображения.</param>
+        ///<param name="k">Вес ошибки.</param>
+        public static float ContentLoss(Tensor X, Tensor C, float k)
+        {
+            float Result = 0.0f;
+            var M = X.Width * X.Height;
+            var N = X.Depth;
+            Parallel.For(0, X.Depth, (int d) =>
+            {
+                var Sum = 0.0f;
+                for(int y = 0; y < X.Height; y++)
+                {
+                    for(int x = 0; x < X.Width; x++)
+                    {
+                        var Delta = X.GetW(x, y, d) - C.GetW(x, y, d);
+                        X.AddDW(x, y, d, (float)((double)Delta / ((long)M * M * N * N) * k));
+                        Sum += Delta * Delta;
+                    }
+                }
+                lock((object)Result)
+                {
+                    Result += (float)((double)Sum / ((long)4 * M * M * N * N) * k);
+                }
+            });
+            return Result;
+        }
+
+    }
+
+}
---
+++ Style Transfer/2015. A Neural Algorithm of Artistic Style/Implementation/src/Network/Optimization/Optimizer.cs
@@ -0,0 +1,119 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//*************************************************************************************************
+
+using System;
+
+namespace NeuralArt
+{
+
+    ///<summary>Оптимизатор Adam.</summary>
+    public static class Adam
+    {
+
+        public static float c
+        {
+            get;
+            set;
+        }
+
+        public static float eps
+        {
+            get;
+            set;
+        }
+
+        public static float beta1
+        {
+            get;
+            set;
+        }
+
+        public static float beta2
+        {
+            get;
+            set;
+        }
+
+        private static float accBeta1
+        {
+            get;
+            set;
+        }
+
+        private static float accBeta2
+        {
+            get;
+            set;
+        }
+
+        private static float oneMinusBeta1
+        {
+            get;
+            set;
+        }
+
+        private static float oneMinusBeta2
+        {
+            get;
+            set;
+        }
+
+        private static float[] accumulatedFirstMoment
+        {
+            get;
+            set;
+        }
+
+        private static float[] accumulatedSecondMoment
+        {
+            get;
+            set;
+        }
+
+        ///<summary>Конструктор Adam. Инициализирует все поля.</summary>
+        static Adam()
+        {
+            c = -10f;
+            eps = 1e-8f;
+            beta1 = 0.9f;
+            beta2 = 0.999f;
+            accBeta1 = beta1;
+            accBeta2 = beta2;
+            oneMinusBeta1 = 1 - beta1;
+            oneMinusBeta2 = 1 - beta2;
+        }
+
+        ///<summary>Выполняет обновление параметров по их градиентам.</summary>
+        public static void Train(Tensor Value)
+        {
+            var oneMinusAccBeta1 = 1 - accBeta1;
+            var oneMinusAccBeta2 = 1 - accBeta2;
+            if(accumulatedFirstMoment == null)
+            {
+                accumulatedFirstMoment = new float[Value.DW.Length];
+            }
+            if(accumulatedSecondMoment == null)
+            {
+                accumulatedSecondMoment = new float[Value.DW.Length];
+            }
+            var gradient = Value.DW;
+            var firstMoment = accumulatedFirstMoment;
+            var secondMoment = accumulatedSecondMoment;
+            for(int i = 0; i < Value.W.Length; i++)
+            {
+                var newFirstMoment = beta1 * firstMoment[i] + oneMinusBeta1 * gradient[i];
+                var newSecondMoment = beta2 * secondMoment[i] + oneMinusBeta2 * gradient[i] * gradient[i];
+                var biasCorrectedFirstMoment = newFirstMoment / oneMinusAccBeta1;
+                var biasCorrectedSecondMoment = newSecondMoment / oneMinusAccBeta2;
+                accumulatedFirstMoment[i] = newFirstMoment;
+                accumulatedSecondMoment[i] = newSecondMoment;
+                var newValue = c * (biasCorrectedFirstMoment / (eps + System.Math.Sqrt(biasCorrectedSecondMoment))) + Value.W[i];
+                Value.W[i] = (float)newValue;
+            }
+            accBeta1 = accBeta1 * beta1;
+            accBeta2 = accBeta2 * beta2;
+        }
+    }
+
+}
