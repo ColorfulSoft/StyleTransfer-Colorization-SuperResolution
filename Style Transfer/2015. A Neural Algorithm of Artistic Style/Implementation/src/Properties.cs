6418f7ddb5a532068a089914edb087be6ec4aaaa
Глеб Брыкин <glebbrykin@yandex.ru>
2019-02-10 10:24:14 +0300

    Update Properties.cs

--- Style Transfer/2015. A Neural Algorithm of Artistic Style/Implementation/src/Properties.cs
+++ Style Transfer/2015. A Neural Algorithm of Artistic Style/Implementation/src/Properties.cs
@@ -6,12 +6,12 @@ using System.Reflection;
 
   //-> Подпись сборки.
 
-  [assembly: AssemblyTitle("NeuralArt")]
+  [assembly: AssemblyTitle("A Neural Algorithm of Artistic Style")]
   [assembly: AssemblyDescription("Image style transfer")]
   [assembly: AssemblyConfiguration("")]
   [assembly: AssemblyVersion("0.09.02.2019")]
   [assembly: AssemblyCompany("ColorfulSoft")]
-  [assembly: AssemblyProduct("NeuralArt")]
+  [assembly: AssemblyProduct("A Neural Algorithm of Artistic Style")]
   [assembly: AssemblyCopyright("© Брыкин Глеб Сергеевич")]
   [assembly: AssemblyTrademark("ImageInstruments")]
   [assembly: AssemblyCulture("")]
