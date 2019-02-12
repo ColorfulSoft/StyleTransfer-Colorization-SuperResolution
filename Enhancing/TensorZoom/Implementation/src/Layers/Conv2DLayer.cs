655f26ba46b71fad1ad512afd9de7f42073da353
Глеб Брыкин <glebbrykin@yandex.ru>
2019-02-12 21:44:12 +0300

    Add files via upload

---
+++ Enhancing/TensorZoom/Implementation/src/Layers/Conv2DLayer.cs
@@ -0,0 +1,108 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//*************************************************************************************************
+
+//-> Определение для слоя Conv2DLayer.
+
+using System;
+using System.Threading.Tasks;
+
+namespace NeuralEnhance
+{
+
+     // В архитектуре сети все свёртки имеют фильтры размером 3x3 и 9x9. Для упрощения кода и оптимизации,
+     // эти значения были интегрированы в код.
+
+     ///<summary>Предоставляет реализацию слоёв нейросети.</summary>
+     public static partial class Layers
+     {
+
+         ///<summary>Реализует слой двумерной свёртки.</summary>
+         ///<param name="input">Входной тензор.</param>
+         ///<param name="Filters">Фильтры.</param>
+         ///<param name="Biases">Смещения.</param>
+         public static Tensor Conv2D3x3(Tensor input, Tensor[] Filters, Tensor Biases)
+         {
+             var OutputDepth = Filters.Length;
+             var Result = new Tensor(input.Width, input.Height, OutputDepth);
+             Parallel.For(0, OutputDepth, (int d) =>
+             {
+                 var f = Filters[d];
+                 for (int ay = 0; ay < input.Height; ay++)
+                 {
+                     var y = ay - 1;
+                     for (int ax = 0; ax < input.Width; ax++)
+                     {
+                         var x = ax - 1;
+                         var a = 0.0f;
+                         for (byte fy = 0; fy < 3; fy++)
+                         {
+                             var oy = y + fy;
+                             for (byte fx = 0; fx < 3; fx++)
+                             {
+                                 var ox = x + fx;
+                                 if ((oy >= 0) && (oy < input.Height) && (ox >= 0) && (ox < input.Width))
+                                 {
+                                     var fi = ((3 * fy) + fx) * f.Depth;
+                                     var ti = ((input.Width * oy) + ox) * input.Depth;
+                                     for (var fd = 0; fd < f.Depth; fd++)
+                                     {
+                                         a += f.W[fi + fd] * input.W[ti + fd];
+                                     }
+                                 }
+                             }
+                         }
+                         a += Biases.W[d];
+                         Result.Set(ax, ay, d, a);
+                     }
+                 }
+             });
+             return Result;
+         }
+
+         ///<summary>Реализует слой двумерной свёртки.</summary>
+         ///<param name="input">Входной тензор.</param>
+         ///<param name="Filters">Фильтры.</param>
+         ///<param name="Biases">Смещения.</param>
+         public static Tensor Conv2D9x9(Tensor input, Tensor[] Filters, Tensor Biases)
+         {
+             var OutputDepth = Filters.Length;
+             var Result = new Tensor(input.Width, input.Height, OutputDepth);
+             Parallel.For(0, OutputDepth, (int d) =>
+             {
+                 var f = Filters[d];
+                 for (int ay = 0; ay < input.Height; ay++)
+                 {
+                     var y = ay - 4;
+                     for (int ax = 0; ax < input.Width; ax++)
+                     {
+                         var x = ax - 4;
+                         var a = 0.0f;
+                         for (byte fy = 0; fy < 9; fy++)
+                         {
+                             var oy = y + fy;
+                             for (byte fx = 0; fx < 9; fx++)
+                             {
+                                 var ox = x + fx;
+                                 if ((oy >= 0) && (oy < input.Height) && (ox >= 0) && (ox < input.Width))
+                                 {
+                                     var fi = ((9 * fy) + fx) * f.Depth;
+                                     var ti = ((input.Width * oy) + ox) * input.Depth;
+                                     for (var fd = 0; fd < f.Depth; fd++)
+                                     {
+                                         a += f.W[fi + fd] * input.W[ti + fd];
+                                     }
+                                 }
+                             }
+                         }
+                         a += Biases.W[d];
+                         Result.Set(ax, ay, d, a);
+                     }
+                 }
+             });
+             return Result;
+         }
+
+     }
+
+}
---
+++ Enhancing/TensorZoom/Implementation/src/Layers/ResidualBlock.cs
@@ -0,0 +1,44 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//*************************************************************************************************
+
+//-> Определение для слоя ResidualBlock.
+
+using System;
+using System.Threading.Tasks;
+
+namespace NeuralEnhance
+{
+
+     ///<summary>Предоставляет реализацию слоёв нейросети.</summary>
+     public static partial class Layers
+     {
+
+         ///<summary>Слой гиперболического тангенса.</summary>
+         ///<param name="input">Входные данные.</param>
+         public static Tensor ResidualBlock(
+                                            Tensor input,
+                                            Tensor[] Filters1,
+                                            Tensor[] Filters2,
+                                            Tensor Biases1,
+                                            Tensor Biases2,
+                                            Tensor Shift1,
+                                            Tensor Shift2,
+                                            Tensor Scale1,
+                                            Tensor Scale2,
+                                            Tensor Mean1,
+                                            Tensor Mean2,
+                                            Tensor Var1,
+                                            Tensor Var2)
+         {
+             var conv = Conv2D3x3(input, Filters1, Biases1);
+             conv = BatchNorm(conv, Shift1, Scale1, Mean1, Var1);
+             conv = ReLU(conv);
+             conv = Conv2D3x3(conv, Filters2, Biases2);
+             conv = BatchNorm(conv, Shift2, Scale2, Mean2, Var2);
+             return ElementwiseSum(conv, input);
+         }
+
+     }
+
+}
