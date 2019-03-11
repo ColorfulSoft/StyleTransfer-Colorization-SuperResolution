a9282bd9c0b4eed7fd23b30b68d388a2e7c06fc6
Глеб Брыкин <glebbrykin@yandex.ru>
2019-03-11 17:51:16 +0300

    Add files via upload

---
+++ Style Transfer/2018. AvatarNet/Implementation/Compile.bat
@@ -0,0 +1,5 @@
+MD "Release\"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe -target:winexe -optimize -unsafe -out:"Release\Avatar-Net.exe" -r:"System.dll" -r:"System.Drawing.dll" -r:"System.Threading.dll" -r:"System.Threading.Tasks.dll" -r:"System.IO.dll" -r:"System.Windows.Forms.dll" -r:"System.Reflection.dll" -resource:"src\Resources\Decoder.model" -resource:"src\Resources\Encoder.model" -resource:"src\Resources\Content.jpg" -resource:"src\Resources\MainIcon.jpg" -resource:"src\Resources\Style.jpg" "src\Data\DecoderData.cs" "src\Data\EncoderData.cs" "src\Interface\MainForm.cs" "src\Layers\AdaINLayer.cs" "src\Layers\AvgPool2DLayer.cs" "src\Layers\Conv2DLayer.cs" "src\Layers\ConvTranspose2DLayer.cs" "src\Layers\ReLULayer.cs" "src\Layers\StyleDecorator.cs" "src\Layers\Upsample2DLayer.cs" "src\Network\Decoder.cs" "src\Network\Encoder.cs" "src\Network\Stylize.cs" "src\Utils\IOConverters.cs" "src\Utils\SVD.cs" "src\Utils\Tensor.cs" "src\Program.cs" "src\Properties.cs"
+
+cmd.exe
