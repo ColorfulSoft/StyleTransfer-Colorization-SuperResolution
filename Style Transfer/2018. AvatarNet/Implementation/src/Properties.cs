3211854c702d0f89f611374265e7c45f06cee47d
Глеб Брыкин <glebbrykin@yandex.ru>
2019-03-11 17:52:42 +0300

    Add files via upload

---
+++ Style Transfer/2018. AvatarNet/Implementation/src/Properties.cs
@@ -0,0 +1,17 @@
+﻿//***********************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//***********************************************
+
+//-> Подпись сборки.
+
+using System.Reflection;
+
+    [assembly: AssemblyTitle("Avatar-Net: Multi-scale Zero-shot Style Transfer by Feature Decoration")]
+    [assembly: AssemblyDescription("Image style transfer")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyVersion("0.10.03.2019")]
+    [assembly: AssemblyCompany("ColorfulSoft")]
+    [assembly: AssemblyProduct("Avatar-Net: Multi-scale Zero-shot Style Transfer by Feature Decoration")]
+    [assembly: AssemblyCopyright("© Брыкин Глеб Сергеевич")]
+    [assembly: AssemblyTrademark("ImageInstruments")]
+    [assembly: AssemblyCulture("")]
