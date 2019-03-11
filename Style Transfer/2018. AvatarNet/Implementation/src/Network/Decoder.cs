d52d23aa3d5b01a96c3b5f83fc8e4a6ccbd86b7c
Глеб Брыкин <glebbrykin@yandex.ru>
2019-03-11 17:57:06 +0300

    Add files via upload

---
+++ Style Transfer/2018. AvatarNet/Implementation/src/Network/Decoder.cs
@@ -0,0 +1,161 @@
+﻿//***********************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//***********************************************
+
+//-> Реализация декодера.
+
+using System;
+using System.IO;
+
+namespace NeuralArt
+{
+
+    ///<summary>Реализует декодирующую сеть.</summary>
+    public sealed class Decoder
+    {
+
+        ///<summary>Данные нейросети.</summary>
+        private DecoderData Data
+        {
+            get;
+            set;
+        }
+
+        ///<summary>Делегат для события выполнения шага через слой нейросети.</summary>
+        ///<param name="percent">Процент выполнения прямого прохода через декодирующую нейросеть.</param>
+        public delegate void StepDone(float percent);
+
+        ///<summary>Инициализирует декодирующую нейросеть, считывая данные из потока.</summary>
+        ///<param name="s">Поток, из которого будет произведено чтение параметров.</param>
+        public Decoder(Stream s)
+        {
+            this.Data = new DecoderData(s);
+        }
+
+        ///<summary>Событие совершения прямого прохода через слой нейросети. Передаёт процент выполнения прохода через всю нейросеть.</summary>
+        public event StepDone Step;
+
+        ///<summary>Выполняет прямой проход через декодирующую нейросеть.</summary>
+        ///<param name="input">Входные данные.</param>
+        public Tensor Decode(Tensor Content, Tensor[] Styles)
+        {
+            var Temp = Layers.StyleDecorator(Content, Styles[3]);
+            Temp = Layers.Upsample2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv4_1_Weights, this.Data.Conv4_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_4_Weights, this.Data.Conv3_4_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_3_Weights, this.Data.Conv3_3_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_2_Weights, this.Data.Conv3_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.AdaIN(Temp, Styles[2]);
+            Temp = Layers.Upsample2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_1_Weights, this.Data.Conv3_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv2_2_Weights, this.Data.Conv2_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.AdaIN(Temp, Styles[1]);
+            Temp = Layers.Upsample2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv2_1_Weights, this.Data.Conv2_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv1_2_Weights, this.Data.Conv1_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.AdaIN(Temp, Styles[0]);
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv1_1_Weights, this.Data.Conv1_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            Temp = Layers.Conv2D7x7(Temp, this.Data.Conv_Out_Weights, this.Data.Conv_Out_Biases);
+            if(this.Step != null)
+            {
+                this.Step(1f / 22f * 100f);
+            }
+            return Temp;
+        }
+
+    }
+
+}
---
+++ Style Transfer/2018. AvatarNet/Implementation/src/Network/Encoder.cs
@@ -0,0 +1,269 @@
+﻿//***********************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//***********************************************
+
+//-> Реализация кодера.
+
+using System;
+using System.IO;
+
+namespace NeuralArt
+{
+
+    ///<summary>Предоставляет кодирующую нейросеть.</summary>
+    public sealed class Encoder
+    {
+
+        ///<summary>Данные нейросети.</summary>
+        private EncoderData Data
+        {
+            get;
+            set;
+        }
+
+        ///<summary>Делегат для события выполнения шага через слой нейросети.</summary>
+        public delegate void StepDone(float percent);
+
+        ///<summary>Инициализирует кодирующую нейросеть, считывая данные из потока.</summary>
+        ///<param name="s">Поток, из которого будет произведено чтение параметров.</param>
+        public Encoder(Stream s)
+        {
+            this.Data = new EncoderData(s);
+        }
+
+        ///<summary>Событие совершения прямого прохода через слой нейросети. Передаёт процент выполнения прохода через всю нейросеть.</summary>
+        public event StepDone Step;
+
+        ///<summary>Выполняет прямой проход через кодирующую нейросеть.</summary>
+        ///<param name="input">Входные данные.</param>
+        public Tensor Encode(Tensor input)
+        {
+            var Temp = input;
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv1_1_Weights, this.Data.Conv1_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv1_2_Weights, this.Data.Conv1_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.AvgPool2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv2_1_Weights, this.Data.Conv2_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv2_2_Weights, this.Data.Conv2_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.AvgPool2D(Temp);
+            if(this.Step != null)
+            {
+                //this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_1_Weights, this.Data.Conv3_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_2_Weights, this.Data.Conv3_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_3_Weights, this.Data.Conv3_3_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_4_Weights, this.Data.Conv3_4_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.AvgPool2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv4_1_Weights, this.Data.Conv4_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            return Temp;
+        }
+
+        ///<summary>Выполняет прямой проход через кодирующую нейросеть.</summary>
+        ///<param name="input">Входные данные.</param>
+        public Tensor[] EncodeStyle(Tensor input)
+        {
+            var Temp = input;
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv1_1_Weights, this.Data.Conv1_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            var Conv1 = Temp;
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv1_2_Weights, this.Data.Conv1_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.AvgPool2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv2_1_Weights, this.Data.Conv2_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            var Conv2 = Temp;
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv2_2_Weights, this.Data.Conv2_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.AvgPool2D(Temp);
+            if(this.Step != null)
+            {
+                //this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_1_Weights, this.Data.Conv3_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            var Conv3 = Temp;
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_2_Weights, this.Data.Conv3_2_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_3_Weights, this.Data.Conv3_3_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv3_4_Weights, this.Data.Conv3_4_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.AvgPool2D(Temp);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.Conv2D3x3(Temp, this.Data.Conv4_1_Weights, this.Data.Conv4_1_Biases);
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            Temp = Layers.ReLU(Temp);
+            var Conv4 = Temp;
+            if(this.Step != null)
+            {
+                this.Step(5f);
+            }
+            return new Tensor[4]{Conv1, Conv2, Conv3, Conv4};
+        }
+
+    }
+
+}
