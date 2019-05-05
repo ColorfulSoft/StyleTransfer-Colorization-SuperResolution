b70926c0fefd279ee4c74e09e2cc093d93f6476e
Глеб Брыкин <glebbrykin@yandex.ru>
2019-05-05 16:04:19 +0300

    Add files via upload

---
+++ Enhancing/Waifu2x/Implementation/src/Layers/LeakyReLULayer.cs
@@ -0,0 +1,40 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//*************************************************************************************************
+
+//-> Определение для слоя LeakyReLULayer.
+
+using System;
+using System.Threading.Tasks;
+
+namespace NeuralEnhance
+{
+
+     ///<summary>Предоставляет реализацию слоёв нейросети.</summary>
+     public static partial class Layers
+     {
+
+         ///<summary>Слой линейной ректификации.</summary>
+         ///<param name="input">Входные данные.</param>
+         public static Tensor LeakyReLU(Tensor input)
+         {
+             var Height = input.Height;
+             var Width = input.Width;
+             var Result = new Tensor(input.Width, input.Height, input.Depth);
+             Parallel.For(0, input.Depth, (int d) =>
+             {
+                 for(int y = 0; y < Height; y++)
+                 {
+                     for(int x = 0; x < Width; x++)
+                     {
+                         var v = input.Get(x, y, d);
+                         Result.Set(x, y, d, (v > 0.0f) ? (v) : (v * (0.1f)));
+                     }
+                 }
+             });
+             return Result;
+         }
+
+     }
+
+}
