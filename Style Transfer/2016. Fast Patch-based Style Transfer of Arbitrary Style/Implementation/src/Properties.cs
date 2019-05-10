484518bf1dea14b28c2d1491915793aaed22a917
Глеб Брыкин <glebbrykin@yandex.ru>
2019-05-10 09:32:01 +0300

    Add files via upload

---
+++ Style Transfer/2016. Fast Patch-based Style Transfer of Arbitrary Style/Implementation/src/Properties.cs
@@ -0,0 +1,17 @@
+﻿//***********************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//***********************************************
+
+//-> Подпись сборки.
+
+using System.Reflection;
+
+    [assembly: AssemblyTitle("Fast Patch-based Style Transfer of Arbitrary Style")]
+    [assembly: AssemblyDescription("Image style transfer")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyVersion("0.04.05.2019")]
+    [assembly: AssemblyCompany("ColorfulSoft")]
+    [assembly: AssemblyProduct("Fast Patch-based Style Transfer of Arbitrary Style")]
+    [assembly: AssemblyCopyright("© Брыкин Глеб Сергеевич")]
+    [assembly: AssemblyTrademark("ImageInstruments")]
+    [assembly: AssemblyCulture("")]
