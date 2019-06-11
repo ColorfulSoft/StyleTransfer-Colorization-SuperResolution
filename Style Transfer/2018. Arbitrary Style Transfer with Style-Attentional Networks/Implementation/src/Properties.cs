fb6401a072c2535990b75a4dc3fb94112bcd5bc4
Глеб Брыкин <glebbrykin@yandex.ru>
2019-06-11 15:02:52 +0300

    Add files via upload

---
+++ Style Transfer/2018. Arbitrary Style Transfer with Style-Attentional Networks/Implementation/src/Properties.cs
@@ -0,0 +1,17 @@
+﻿//***********************************************
+//* (C) ColorfulSoft, 2019. Все права защищены.
+//***********************************************
+
+//-> Подпись сборки.
+
+using System.Reflection;
+
+    [assembly: AssemblyTitle("Arbitrary Style Transfer with Style-Attentional Networks")]
+    [assembly: AssemblyDescription("Image style transfer")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyVersion("0.11.06.2019")]
+    [assembly: AssemblyCompany("ColorfulSoft")]
+    [assembly: AssemblyProduct("Arbitrary Style Transfer with Style-Attentional Networks")]
+    [assembly: AssemblyCopyright("© Брыкин Глеб Сергеевич")]
+    [assembly: AssemblyTrademark("ImageInstruments")]
+    [assembly: AssemblyCulture("")]
