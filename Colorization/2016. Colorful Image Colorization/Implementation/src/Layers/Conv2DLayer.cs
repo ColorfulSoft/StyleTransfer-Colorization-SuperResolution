0211029c29760bf64600455b6d059316fa560bd4
Глеб Брыкин <glebbrykin@yandex.ru>
2019-10-16 20:55:50 +0300

    Add files via upload

--- Colorization/2016. Colorful Image Colorization/Implementation/src/Layers/Conv2DLayer.cs
+++ Colorization/2016. Colorful Image Colorization/Implementation/src/Layers/Conv2DLayer.cs
@@ -14,35 +14,6 @@ namespace NeuralColor
     public static partial class Layers
     {
 
-        private static Tensor[] DilateFilters(Tensor[] T, int dilation = 1)
-        {
-            if(dilation == 1)
-            {
-                return T;
-            }
-            else
-            {
-                var Result = new Tensor[T.Length];
-                Parallel.For(0, T.Length, (int i) =>
-                {
-                    Result[i] = new Tensor(T[i].Width * dilation + dilation - 1, T[i].Height * dilation + dilation - 1, T[i].Depth);
-                    var F = Result[i];
-                    var f = T[i];
-                    for(int y = 0; y < f.Height; y++)
-                    {
-                        for(int x = 0; x < f.Width; x++)
-                        {
-                            for(int d = 0; d < f.Depth; d++)
-                            {
-                                F.Set(x * dilation + dilation - 1, y * dilation + dilation - 1, d, f.Get(x, y, d));
-                            }
-                        }
-                    }
-                });
-                return Result;
-            }
-        }
-
         ///<summary>Реализует слой двумерной свёртки.</summary>
         ///<param name="input">Входной тензор.</param>
         ///<param name="Filters">Фильтры.</param>
@@ -51,7 +22,6 @@ namespace NeuralColor
         ///<param name="dilation">Разрежение.</param>
         public static Tensor Conv2D(Tensor input, Tensor[] Filters, Tensor Biases, int stride = 1, int dilation = 1)
         {
-            Filters = DilateFilters(Filters, dilation);
             var OutputDepth = Filters.Length;
             var Result = new Tensor(input.Width / stride, input.Height / stride, OutputDepth);
             Parallel.For(0, OutputDepth, (int d) =>
@@ -59,17 +29,17 @@ namespace NeuralColor
                 var f = Filters[d];
                 for (int ay = 0; ay < Result.Height; ay++)
                 {
-                    var y = ay * stride - f.Height / 2;
+                    var y = ay * stride - (f.Height * dilation + dilation - 1) / 2;
                     for (int ax = 0; ax < Result.Width; ax++)
                     {
-                        var x = ax * stride - f.Width / 2;
+                        var x = ax * stride - (f.Width * dilation + dilation - 1) / 2;
                         var a = 0.0f;
                         for (int fy = 0; fy < f.Height; fy++)
                         {
-                            var oy = y + fy;
+                            var oy = y + fy * dilation + dilation - 1;
                             for (int fx = 0; fx < f.Width; fx++)
                             {
-                                var ox = x + fx;
+                                var ox = x + fx * dilation + dilation - 1;
                                 if ((oy >= 0) && (oy < input.Height) && (ox >= 0) && (ox < input.Width))
                                 {
                                     var fi = ((f.Width * fy) + fx) * f.Depth;
