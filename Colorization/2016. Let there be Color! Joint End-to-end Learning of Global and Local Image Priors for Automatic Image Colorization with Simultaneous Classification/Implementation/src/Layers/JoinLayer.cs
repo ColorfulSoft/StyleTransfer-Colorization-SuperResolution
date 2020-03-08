4a6cef6e1b1db7e5d659502e71fc1a027b1793df
Глеб Брыкин <glebbrykin@yandex.ru>
2020-03-08 15:33:57 +0300

    Add files via upload

---
+++ Colorization/2016. Let there be Color! Joint End-to-end Learning of Global and Local Image Priors for Automatic Image Colorization with Simultaneous Classification/Implementation/src/Layers/JoinLayer.cs
@@ -0,0 +1,51 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2020. Все права защищены.
+//*************************************************************************************************
+
+//-> Определение для слоя JoinLayer.
+
+using System;
+using System.Threading.Tasks;
+
+namespace NeuralColor
+{
+
+     ///<summary>Предоставляет реализацию слоёв нейросети.</summary>
+     public static partial class Layers
+     {
+
+         ///<summary>Слой поэлементного умножения.</summary>
+         ///<param name="input">Входные данные.</param>
+         public static Tensor JoinLayer(Tensor input, Tensor joint)
+         {
+             var Height = input.Height;
+             var Width = input.Width;
+             var Result = new Tensor(input.Width, input.Height, input.Depth + joint.Depth);
+             Parallel.For(0, input.Depth, (int d) =>
+             {
+                 for(int y = 0; y < Height; y++)
+                 {
+                     for(int x = 0; x < Width; x++)
+                     {
+                         var v = input.Get(x, y, d);
+                         Result.Set(x, y, d, v);
+                     }
+                 }
+             });
+             Parallel.For(0, joint.Depth, (int d) =>
+             {
+                 var v = joint.Get(0, 0, d);
+                 for(int y = 0; y < Height; y++)
+                 {
+                     for(int x = 0; x < Width; x++)
+                     {
+                         Result.Set(x, y, d + input.Depth, v);
+                     }
+                 }
+             });
+             return Result;
+         }
+
+     }
+
+}
---
+++ Colorization/2016. Let there be Color! Joint End-to-end Learning of Global and Local Image Priors for Automatic Image Colorization with Simultaneous Classification/Implementation/src/Layers/LinearLayer.cs
@@ -0,0 +1,47 @@
+﻿//*************************************************************************************************
+//* (C) ColorfulSoft, 2020. Все права защищены.
+//*************************************************************************************************
+
+//-> Определение для слоя LinearLayer.
+
+using System;
+using System.Threading.Tasks;
+
+namespace NeuralColor
+{
+
+     ///<summary>Предоставляет реализацию слоёв нейросети.</summary>
+     public static partial class Layers
+     {
+
+         ///<summary>Слой поэлементного умножения.</summary>
+         ///<param name="input">Входные данные.</param>
+         public static Tensor LinearLayer(Tensor input, Tensor[] weights, Tensor biases)
+         {
+             var Height = input.Height;
+             var Width = input.Width;
+             var Result = new Tensor(1, 1, weights.Length);
+             Parallel.For(0, weights.Length, (int d) =>
+             {
+                 var f = weights[d];
+                 var a = 0f;
+                 var i = 0;
+                 for(int id = 0; id < input.Depth; id++)
+                 {
+                     for(int y = 0; y < Height; y++)
+                     {
+                         for(int x = 0; x < Width; x++)
+                         {
+                             a += input.Get(x, y, id) * f.W[i];
+                             i += 1;
+                         }
+                     }
+                 }
+                 Result.W[d] = a + biases.W[d];
+             });
+             return Result;
+         }
+
+     }
+
+}
