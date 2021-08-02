141c123251eca98f338aa94c3355aa435d7167ab
Alvin Seville <alvinseville7cf@gmail.com>
2021-08-02 20:00:43 +1000

    Cosmetic fixes
    - @echo off added
    - formatting enhanced
    - environment variables used

--- Colorization/2016. Colorful Image Colorization/Implementation/Compile.bat
+++ Colorization/2016. Colorful Image Colorization/Implementation/Compile.bat
@@ -1,14 +1,20 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe -target:exe -optimize -DEBUG -out:"BuildWeights.exe" -r:"System.dll" -r:"System.IO.dll" "BuildWeights.cs"
+@echo off
 
-BuildWeights.exe
+set "RELEASE=Release\"
 
-DEL /F /Q "BuildWeights.exe"
+del /f /q /s "%RELEASE%"
 
-DEL /F /Q "BuildWeights.pdb"
+csc.exe -target:exe -optimize -DEBUG -out:"BuildWeights.exe" -r:"System.dll" -r:"System.IO.dll" "BuildWeights.cs" && BuildWeights.exe
+del /f /q "BuildWeights.*"
 
-MOVE "ColorfulImageColorization.model" "src\Resources\"
+move "ColorfulImageColorization.model" "src\Resources\"
+md "%RELEASE%"
 
-MD "Release\"
-
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe -target:winexe -optimize -unsafe -out:"Release\Colorful Image Colorization.exe" -r:"System.dll" -r:"System.Drawing.dll" -r:"System.Threading.dll" -r:"System.Threading.Tasks.dll" -r:"System.IO.dll" -r:"System.Windows.Forms.dll" -r:"System.Reflection.dll" -resource:"src\Resources\ColorfulImageColorization.model" -resource:"src\Resources\MainIcon.jpg" -resource:"src\Resources\Original.jpg" "src\Data\Data.cs" "src\Interface\MainForm.cs" "src\Layers\BatchNormLayer.cs" "src\Layers\Conv2DLayer.cs" "src\Layers\ConvTranspose2DLayer.cs" "src\Layers\ElementwiseMulLayer.cs" "src\Layers\ReLULayer.cs" "src\Layers\SoftmaxLayer.cs" "src\Network\ColorfulColorization.cs" "src\Utils\IOConverters.cs" "src\Utils\Tensor.cs" "src\Program.cs"
-cmd.exe
+csc.exe -target:winexe -optimize -unsafe -out:"%RELEASE%\Colorful Image Colorization.exe" ^
+  -r:"System.dll" -r:"System.Drawing.dll" -r:"System.Threading.dll" -r:"System.Threading.Tasks.dll" ^
+  -r:"System.IO.dll" -r:"System.Windows.Forms.dll" -r:"System.Reflection.dll" -resource:"src\Resources\ColorfulImageColorization.model" ^
+  -resource:"src\Resources\MainIcon.jpg" -resource:"src\Resources\Original.jpg" "src\Data\Data.cs" ^
+  "src\Interface\MainForm.cs" "src\Layers\BatchNormLayer.cs" "src\Layers\Conv2DLayer.cs" "src\Layers\ConvTranspose2DLayer.cs" ^
+  "src\Layers\ElementwiseMulLayer.cs" "src\Layers\ReLULayer.cs" "src\Layers\SoftmaxLayer.cs" "src\Network\ColorfulColorization.cs" ^
+  "src\Utils\IOConverters.cs" "src\Utils\Tensor.cs" "src\Program.cs"
+pause
