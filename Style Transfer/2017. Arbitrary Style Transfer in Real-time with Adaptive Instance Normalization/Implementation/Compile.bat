7e685a0f7826fa1eeeee8edd216a2cbbdc1c580a
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2021-11-03 21:08:51 +0300

    Add files via upload

--- Style Transfer/2017. Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization/Implementation/Compile.bat
+++ Style Transfer/2017. Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization/Implementation/Compile.bat
@@ -1,5 +1,3 @@
-MD Release
-
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe -target:winexe -optimize -unsafe -out:"Release\Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization.exe" -r:"System.dll" -r:"System.Drawing.dll" -r:"System.Threading.dll" -r:"System.Threading.Tasks.dll" -r:"System.IO.dll" -r:"System.Windows.Forms.dll" -r:"System.Reflection.dll" -resource:"src\Resources\Encoder.HModel" -resource:"src\Resources\Decoder.HModel" -resource:"src\Resources\MainIcon.jpg" -resource:"src\Resources\Style.jpg" -resource:"src\Resources\Content.jpg" "src\Data\DecoderData.cs" "src\Data\EncoderData.cs" "src\Interface\MainForm.cs" "src\Layers\AdaINLayer.cs" "src\Layers\Conv2DLayer.cs" "src\Layers\MaxPool2DLayer.cs" "src\Layers\ReLULayer.cs" "src\Layers\Upsample2DLayer.cs" "src\Network\Decoder.cs" "src\Network\Encoder.cs" "src\Utils\Half2Float.cs" "src\Utils\IOConverters.cs" "src\Utils\Tensor.cs" "src\Program.cs" "src\Properties.cs"
-
+MD "Release"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"Windows.rsp"
 cmd.exe
